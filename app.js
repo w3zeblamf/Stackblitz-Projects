493f140e4599169de8738aa9b7b1d4f5d32f0aa5
w3zeblamf <zeblamfw3@gmail.com>
2022-07-07 23:15:42 -0400

    file added

--- app.js
+++ app.js
@@ -1,18 +1,4 @@
-const toggleBtn = document.querySelector('.sidebar-toggle');
-const closeBtn = document.querySelector('.close-btn');
-const sidebar = document.querySelector('.sidebar');
-
-toggleBtn.addEventListener('click', function () {
-  // if (sidebar.classList.contains("show-sidebar")) {
-  //   sidebar.classList.remove("show-sidebar");
-  // } else {
-  //   sidebar.classList.add("show-sidebar");
-  // }
-
-//The same functionality using 'toggle'
-  sidebar.classList.toggle('show-sidebar');
-});
-
-closeBtn.addEventListener('click', function () {
-  sidebar.classList.remove('show-sidebar');
-});
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
