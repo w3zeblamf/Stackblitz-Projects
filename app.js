d6449522d250a3211db67c0312f2f2aa83f20cdc
w3zeblamf <zeblamfw3@gmail.com>
2022-07-09 20:42:35 -0400

    file updated

--- app.js
+++ app.js
@@ -72,3 +72,15 @@ const menu = [
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
+
+const sectionCenter = document.querySelector('.sectionCenter');
+
+window.addEventListener('DOMContentLoaded', function() {
+  let displayMenu = menu.map(function (item) {
+    console.log(item);
+
+    return item;
+  });
+
+  console.log(displayMenu);
+})
