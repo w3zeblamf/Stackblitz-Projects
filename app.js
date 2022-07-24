e394304ae658aacb882b3a10e5cc67ed9ee2d2e1
w3zeblamf <zeblamfw3@gmail.com>
2022-07-24 17:07:22 -0400

    1 file modified

--- app.js
+++ app.js
@@ -24,6 +24,14 @@ function addItem(e) {
 
   const value = grocery.value;
   const id = new Date().getTime().toString();
+
+  if (value !== '' && editFlag === false) {
+    console.log('add item to the list');
+  } else if (value !== '' && editFlag === true) {
+    console.log('editing');
+  } else {
+    console.log('empty value');
+  }
 }
 // ****** LOCAL STORAGE **********
 
