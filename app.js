925e1f48f1bb7e4fe5cc15bf41ae7ddfbba66ca5
w3zeblamf <zeblamfw3@gmail.com>
2022-09-08 20:47:39 -0400

    2 files modified

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const app = Vue.createApp({
       vueLink: 'https://vuejs.org',
     };
   },
+
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return 'Learn Vue!';
+      } else {
+        return 'Master Vue!';
+      }
+    },
+  },
 });
 
 app.mount('#user-goal');
